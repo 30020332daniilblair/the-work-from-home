ba73a1e43d572c53a2e24487431564d43f8d8378
Daniil Blair 30020332 <48031789+30020332daniilblair@users.noreply.github.com>
2019-03-06 11:35:16 +1300

    Add files via upload

---
+++ VS work for computing.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("What is your first name" ); 
+            string fname = Console.ReadLine();
+            Console.WriteLine("What is your last name");
+            string Lname = Console.ReadLine();
+            Console.WriteLine("{0} {1}", fname, Lname);
+            Console.ReadLine(); */
+            /*
+            Console.WriteLine("Please write your name");
+            string fname = Console.ReadLine();
+            Console.WriteLine("please write your last name");
+            string lname = Console.ReadLine();
+            Console.WriteLine("please write your adress");
+            string adress = Console.ReadLine();
+            Console.WriteLine("what is your age");
+            string age = Console.ReadLine();
+            Console.WriteLine("what is your email addy");
+            string addy = Console.ReadLine();
+            Console.WriteLine("{0} {1} {2} {3} {4}", fname, lname, adress, age, addy);
+            Console.ReadLine(); */
+            
+            Console.WriteLine("Please write your First name");
+            string fname = Console.ReadLine();
+            Console.WriteLine("please write your last name");
+            string lname = Console.ReadLine();
+            Console.WriteLine("please write your addy");
+            string addy = Console.ReadLine();
+            Console.WriteLine("Please right your age");
+            string age = Console.ReadLine();
+            Console.WriteLine("Please write your email address");
+            string email = Console.ReadLine();
+            Console.WriteLine("Please enter a four digit pin");
+            string pin = Console.ReadLine();
+            Console.WriteLine("{0} {1} {2} {3} {4} {5}", fname,  lname, addy, age, email, pin);
+            
+            
+            
+            
+            
+
+            
+            
+ 
+               
+            
+
+        }
+    }
+}
